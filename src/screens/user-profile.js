c4fa5f446c103b143e0098f0946713aa0e77bf16
Les Ansley <les@ansleyfamily.com>
2022-11-23 15:01:08 +0200

    Create user-profile.js

---
+++ src/screens/user-profile.js
@@ -0,0 +1,90 @@
+import {UserProvider, useUser, updateUser} from './context/user-context'
+
+function UserSettings() {
+  const [{user, status, error}, userDispatch] = useUser()
+
+  const isPending = status === 'pending'
+  const isRejected = status === 'rejected'
+
+  const [formState, setFormState] = React.useState(user)
+
+  const isChanged = !dequal(user, formState)
+
+  function handleChange(e) {
+    setFormState({...formState, [e.target.name]: e.target.value})
+  }
+
+  function handleSubmit(event) {
+    event.preventDefault()
+    updateUser(userDispatch, user, formState).catch(error)
+  }
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <div style={{marginBottom: 12}}>
+        <label style={{display: 'block'}} htmlFor="username">
+          Username
+        </label>
+        <input
+          id="username"
+          name="username"
+          disabled
+          readOnly
+          value={formState.username}
+          style={{width: '100%'}}
+        />
+      </div>
+      <div style={{marginBottom: 12}}>
+        <label style={{display: 'block'}} htmlFor="tagline">
+          Tagline
+        </label>
+        <input
+          id="tagline"
+          name="tagline"
+          value={formState.tagline}
+          onChange={handleChange}
+          style={{width: '100%'}}
+        />
+      </div>
+      <div style={{marginBottom: 12}}>
+        <label style={{display: 'block'}} htmlFor="bio">
+          Biography
+        </label>
+        <textarea
+          id="bio"
+          name="bio"
+          value={formState.bio}
+          onChange={handleChange}
+          style={{width: '100%'}}
+        />
+      </div>
+      <div>
+        <button
+          type="button"
+          onClick={() => {
+            setFormState(user)
+            userDispatch({type: 'reset'})
+          }}
+          disabled={!isChanged || isPending}
+        >
+          Reset
+        </button>
+        <button
+          type="submit"
+          disabled={(!isChanged && !isRejected) || isPending}
+        >
+          {isPending
+            ? '...'
+            : isRejected
+            ? '✖ Try again'
+            : isChanged
+            ? 'Submit'
+            : '✔'}
+        </button>
+        {isRejected ? <pre style={{color: 'red'}}>{error.message}</pre> : null}
+      </div>
+    </form>
+  )
+}
+
+export default UserSettings
